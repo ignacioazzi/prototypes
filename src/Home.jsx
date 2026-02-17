9f8f368a7dc17bcaf9f11425694cfb8cb7eb7941
ignacioazzi <ignacioazzi@gmail.com>
2026-02-17 02:25:51 +0100

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,12 +6,10 @@ export default function Home() {
   };
 
   return (
-    <div style={{ width: '390px', aspectRatio: '1 / 2.13', transform: 'scale(0.9)' }} className=" flex bg-gray-50 rounded-3xl shadow-2xl  flex-col items-center justify-center bg-gray-100 gap-16 p-8">
+    <div style={{ width: '390px', aspectRatio: '1 / 2.13', transform: 'scale(0.9)' }} className=" flex rounded-3xl shadow-2xl  flex-col items-center justify-center bg-white  gap-8 p-8">
 
-
+      <h1 className="text-2xl font-bold">Prototypes</h1>
       <nav className="flex flex-col gap-4">
-
-
         <button
           onClick={() => handleNavigation('#full')}
           className="px-8 py-3 bg-blue-600 text-white font-semibold rounded-lg "
